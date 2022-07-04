a601a486fd40904e11a70913ae20b6e7221871cf
george lucas <glalmeida.dev@gmail.com>
2022-07-04 18:20:12 -0300

    backend: rota post criada

--- backend/controllers/tarefaController.js
+++ backend/controllers/tarefaController.js
@@ -2,12 +2,28 @@ const tarefaService = require('../services/tarefaService');
 
 const tarefaController = async (req, res) => {
     try {
-    const resultsService = await tarefaService();
+    const resultsService = await tarefaService.tarefaService();
     return res.status(200).json(resultsService);
     } catch (error) {
         console.error(error.message);
-        res.status(500).send("deu ruim")
+        res.status(500).send("deu ruim rota-get")
     }
 }
 
-module.exports = tarefaController;
+const tarefaControllerPost = async (req, res) => {
+    try {
+        const { name, status } = req.body;
+        const data = new Date();
+        const dataCriacao = data.toLocaleDateString('pt-BR', {timeZone: 'UTC'});
+        const resultServicePost = await tarefaService.tarefaServicePost(name, dataCriacao, status);
+        return res.status(201).json(resultServicePost);
+    } catch (error) {
+        console.error(error.message);
+        res.status(500).send("deu ruim rota-post")
+    }
+}
+
+module.exports = {
+    tarefaController,
+    tarefaControllerPost
+};
